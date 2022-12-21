ee2ee239627c51904534250d737f5864e8adff3d
only-nico <nicolas.villegas01@alumnos.uach.cl>
2022-12-21 00:03:45 -0300

    ultimo comitazo del nico

--- src/Publicacion.js
+++ src/Publicacion.js
@@ -1,7 +1,7 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {useState,useEffect} from 'react';
 
-const CLIENT_ID="b01669ce06464e06ad1afe9c395c7c15";
+const CLIENT_ID="b01669ce06464e06ad1afe9c395c7c15"; //id del cliente 
 const CLIENT_SECRET="c7e50afc03dc417ca2181cf3d81664ff";
 
 export default function Publicacion({setSongs}){
@@ -22,21 +22,19 @@ export default function Publicacion({setSongs}){
           .then(data=>setAccessToken(data.access_token))
       },[])
 
-      async function search(){
-        console.log("search for "+searchInput); 
-        
-        var artistParameters={
-          method: 'GET',
+      async function search(){//funcion para buscar por artista o por la cancion
+        var artistParameters={//si es que la persona solo pone la cancion
+          method: 'GET',      //en el selector de canciones la cancion sera la primera
           headers:{
             'Content-Type': 'application/json',
             'Authorization': 'Bearer '+accessToken
           }
         }
-        var artistID =await fetch('https://api.spotify.com/v1/search?q='+searchInput+'&type=track%2Cartist',artistParameters)
+        var artistID =await fetch('https://api.spotify.com/v1/search?q='+searchInput+'&type=track%2Cartist',artistParameters)//se hace la busqueda
         .then(response=> response.json())
-        .then(data=>{return data.artists.items[0].id && data.tracks})
+        .then(data=>{return data.artists.items[0].id && data.tracks})//retorna el primer item que encuentra y sus canciones
         
-        setSongs(artistID.items);
+        setSongs(artistID.items);//setea las songs
       }
     
     return(   
@@ -53,8 +51,8 @@ export default function Publicacion({setSongs}){
                           
                     
                           onChange={event=>setSearchInput(event.target.value)}
-                          onBlur={search}
-                          />
+                          onBlur={search}//busca la cancion por artista cuando se deja de focusear el div, es decir cuando se cambia de div
+                          />              
                     </div>
                     </form>
                 </div>
